f7763994ed03398900e979a852c55425ace14567
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-08-03 22:44:24 +1000

    Basic page testing
    - Links
    - Alt Text

--- tests/page.spec.ts
+++ tests/page.spec.ts
@@ -1,7 +1,31 @@
 import { test, expect } from "@playwright/test";
 
-test("Check title", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+const website: string = "http://localhost:5173/";
 
-  await expect(page).toHaveTitle(/Kloner/);
+test.describe("Check website", () => {
+  test("Check title", async ({ page }) => {
+    await page.goto(website);
+
+    await expect(page).toHaveTitle(/Kloner/);
+  });
+
+  test("Check alt text", async ({ page }) => {
+    await page.goto(website);
+    await expect(page.getByRole("img")).toHaveAttribute("alt");
+  });
+});
+
+test.describe("Check Links", () => {
+  test("Check email", async ({ page }) => {
+    await page.goto(website);
+    await expect(page.getByText("Email")).toHaveAttribute(
+      "href",
+      /mailto:bunyan.keiran@gmail.com/
+    );
+  });
+  test("Check GitHub", async ({ page }) => {
+    await page.goto(website);
+    await page.getByRole("link", { name: "GitHub" }).click({ force: true });
+    await expect(page).toHaveURL("https://github.com/Klon3r");
+  });
 });
