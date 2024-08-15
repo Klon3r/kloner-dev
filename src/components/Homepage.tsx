0bd7f092be460ac9f80e132c04dc9b644b5f17e4
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-08-16 03:01:19 +1000

    Added terminal interface
    - Need to work on commands

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,70 +1,65 @@
-import Image from "../assets/photo-me-greyscale.png";
+import React, { useEffect, useMemo } from "react";
+import { Terminal } from "../Terminal";
+import { useTerminal } from "../Terminal/hooks";
 
 function Homepage() {
+  const { history, pushToHistory, setTerminalRef, resetTerminal } =
+    useTerminal();
+
+  useEffect(() => {
+    resetTerminal();
+
+    pushToHistory(
+      <>
+        <div style={{ fontSize: 20 }}>
+          <strong>kloner.dev</strong>
+        </div>
+        <div>Welcome to my website, it is kinda a terminal</div>
+        <br />
+        <div>To get started please enter to following commands</div>
+        <div>Commands: about-me</div>
+      </>
+    );
+  }, []);
+
+  const commands = useMemo(
+    () => ({
+      start: async () => {
+        await pushToHistory(
+          <>
+            <div>
+              <strong>Starting</strong> the server...{" "}
+              <span style={{ color: "green" }}>Done</span>
+            </div>
+          </>
+        );
+      },
+      alert: async () => {
+        alert("Hello!");
+        await pushToHistory(
+          <>
+            <div>
+              <strong>Alert</strong>
+              <span style={{ color: "orange", marginLeft: 10 }}>
+                <strong>Shown in the browser</strong>
+              </span>
+            </div>
+          </>
+        );
+      },
+    }),
+    [pushToHistory]
+  );
+
   return (
-    <>
-      <div id="main-div">
-        <div id="left-div">
-          <img id="photo-me" alt="A photo of Keiran Bunyan" src={Image}></img>
-        </div>
-        <div id="center-div">
-          <div id="title"> About Me</div>
-          <div id="content">
-            <p>
-              Hello, my name is Keiran Bunyan. I am a passionate web developer
-              who loves programming and problem-solving. I thrive on the
-              challenge of turning problems into elegant, functional solutions.
-            </p>
-            <p>
-              I have recently graduated with a Bachelor's degree in 'Computer
-              Science'. My academic journey provided me with a comprehensive
-              foundation of software development, data structures & algorithms.
-            </p>
-            <p>
-              I am committed to continuously enhancing my knowledge and
-              deepening my interest in web technologies.
-            </p>
-            <p>
-              My goal is to stay of the forefront of industry trends and
-              continually improve my skills to create innovative and effective
-              web solutions.
-            </p>
-          </div>
-        </div>
-        <div id="right-div">
-          <div id="inner-div">
-            <div id="title">Skills</div>
-            <div id="content">
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Javascript</li>
-                <li>React</li>
-                <li>Python</li>
-                <li>SQL</li>
-              </ul>
-              <ul>
-                <li>Git/GitHub</li>
-                <li>CI/CD</li>
-                <li>Testing</li>
-                <li>Linux</li>
-              </ul>
-            </div>
-          </div>
-          <div id="inner-div">
-            <div id="links">
-              <div id="title">Links</div>
-              <div id="content">
-                <ul id="link-ul">
-                  <a href="mailto:bunyan.keiran@gmail.com">Email</a>
-                  <a href="https://github.com/Klon3r">GitHub</a>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="Homepage">
+      <Terminal
+        history={history}
+        ref={setTerminalRef}
+        promptLabel={<>{">"}</>}
+        commands={commands}
+      />
+    </div>
   );
 }
 
---
+++ src/components/PhotoMe.tsx
@@ -0,0 +1,11 @@
+import Image from "../assets/photo-me-greyscale.png";
+
+function PhotoMe() {
+  <>
+    <div id="left-div">
+      <img id="photo-me" alt="A photo of Keiran Bunyan" src={Image}></img>
+    </div>
+  </>;
+}
+
+export default PhotoMe;
