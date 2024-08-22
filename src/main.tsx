89404188994a684046292e73f060c1b026346c38
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-08-22 20:40:53 +1000

    Reformated code for fresh start

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Homepage from "./components/Homepage.tsx";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Homepage />
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 );
