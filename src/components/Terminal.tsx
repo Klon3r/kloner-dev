37d6ad02640c4a29430a3efd00df9388749b92df
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-09-08 17:20:24 +1000

    Updated commands
    - Added about-me command
    - Added auto-scrolling for text in the terminal
    - Added '/n' to the start of setOutput for better splitting

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,12 +1,20 @@
-import SlowType from "./useSlowType";
-import { useState } from "react";
+import useSlowType from "./useSlowType";
+import { useEffect, useRef, useState } from "react";
 
 function Terminal() {
   const [commandInput, setCommandInput] = useState("");
   const [output, setOutput] = useState("");
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const shouldAutoScroll = useRef(true);
+  const [isComplete, setIsComplete] = useState(false);
+
+  const handleTypingComplete = () => {
+    setIsComplete(true);
+    shouldAutoScroll.current = false;
+  };
 
   // Custom hook usage
-  const lines = SlowType(output, 10);
+  const lines = useSlowType(output, 3, handleTypingComplete);
 
   function changeInput(e: string) {
     setCommandInput(e);
@@ -25,25 +33,47 @@ function Terminal() {
 
     switch (value) {
       case "help":
-        setOutput("List of Commands: clear, email\n");
+        setOutput("\nList of Commands: clear, email\n");
+        resetScroll();
         break;
       case "clear":
-        setOutput("clear");
+        setOutput("clear"); // Clear output
+        resetScroll();
         break;
       case "email":
-        setOutput("bunyan.keiran (at) gmail.com\n");
+        setOutput("\nbunyan.keiran@gmail.com\n");
+        resetScroll();
+        break;
+      case "about-me":
+        setOutput(
+          `\nHello, my name is Keiran Bunyan. I am a passionate web developer who loves programming and problem-solving. I thrive on the challenge of turning problems into elegant, functional solutions.\nI have recently graduated with a Bachelor's degree in 'Computer Science'. My academic journey provided me with a comprehensive foundation of software development, data structures & algorithms.\nI am committed to continuously enhancing my knowledge and deepening my interest in web technologies.\nMy goal is to stay of the forefront of industry trends and continually improve my skills to create innovative and effective web solutions.`
+        );
+        resetScroll();
         break;
       default:
-        setOutput(`Unknown command: ${value}\n`);
+        setOutput(`\nUnknown command: ${value}\n`);
     }
   }
+
+  const resetScroll = () => {
+    shouldAutoScroll.current = true;
+    setIsComplete(false);
+  };
+
+  useEffect(() => {
+    if (shouldAutoScroll.current && containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  }, [lines, isComplete]); // Run on line change or complete
 
   return (
     <>
       <div id="command-div">Type 'help' for a list of commands</div>
-      <div id="terminal-body">
+      <div id="terminal-body" ref={containerRef}>
         {lines.map((line, index) => (
-          <div key={index}>{line}</div>
+          <div key={index} style={{ marginBottom: "1em", paddingLeft: "1em" }}>
+            {line}
+          </div>
         ))}
       </div>
       <div id="terminal-input-div">
--- src/components/useSlowType.tsx
+++ src/components/useSlowType.tsx
@@ -1,22 +1,44 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-function useSlowType(text: string, speed: number) {
+function useSlowType(text: string, speed: number, onComplete: () => void) {
   const [outputText, setOutputText] = useState("");
+  const typingRef = useRef(true);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    const typeText = () => {
-      if (text === "clear") {
-        setOutputText("");
-        return "";
+    if (text === "clear") {
+      setOutputText("");
+      onComplete();
+      typingRef.current = false;
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = null;
       }
+      return;
+    }
 
-      let timeoutId: NodeJS.Timeout;
+    typingRef.current = true;
+
+    const typeText = () => {
       for (let i = 0; i < text.length; i++) {
-        timeoutId = setTimeout(() => {
+        if (!typingRef.current) {
+          return;
+        }
+        timeoutRef.current = setTimeout(() => {
           setOutputText((prevText) => prevText + text.charAt(i));
+
+          // onComplete if on last character
+          if (i === text.length - 1) {
+            onComplete();
+          }
         }, i * speed);
       }
-      return () => clearTimeout(timeoutId);
+      return () => {
+        typingRef.current = false;
+        if (timeoutRef.current) {
+          clearTimeout(timeoutRef.current);
+        }
+      };
     };
 
     typeText();
