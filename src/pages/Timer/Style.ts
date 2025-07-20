d85d05a9e765933b5bab210b9c6292d4e34cb680
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-07-20 20:49:11 +1000

    Increased button sizing

--- src/pages/Timer/Style.ts
+++ src/pages/Timer/Style.ts
@@ -4,7 +4,7 @@ export const timerButton = tw(
   "flex items-center justify-center border-1 rounded-sm p-3 leading-none hover:cursor-pointer"
 );
 export const minuteButton = tw(
-  "flex items-center justify-center rounded-sm p-2 leading-none hover:cursor-pointer text-2xl"
+  "flex items-center justify-center rounded-sm p-2 leading-none hover:cursor-pointer text-3xl"
 );
 export const timerButtonContainer = tw("flex gap-10 pt-10");
 
