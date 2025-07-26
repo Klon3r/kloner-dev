8345bb0b61c9a71d9f28661e127b44f5a5818c4e
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-07-26 13:25:12 +1000

    Update theme/card styling

--- src/components/Card/Style.ts
+++ src/components/Card/Style.ts
@@ -1,13 +1,13 @@
 const tw = (classes: string) => classes;
 
-export const hoverImageStyle = tw("hover:rounded-xl hover:cursor-pointer");
+export const hoverImageStyle = tw("hover:scale-105 hover:cursor-pointer");
 
 export const imageStyle = tw(
-  "!max-w-none !w-80 !shadow-none h-auto !border-1 !border-purple-400 !m-0"
+  "!max-w-none !w-80 h-auto !border-1 !border-purple-400 !m-0"
 );
 
 export const mobileImageStyle = tw(
-  "!max-w-none !w-60 !shadow-none h-auto !border-1 !border-purple-400 !m-0"
+  "!max-w-none !w-60 h-auto !border-1 !border-purple-400 !m-0 shadow-2xl"
 );
 
 export const divStyle = tw("flex justify-center items-center");
