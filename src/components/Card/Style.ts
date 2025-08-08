113207cf1454d660171e4579a9875535549677e5
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-08-09 00:10:52 +1000

    Updated card border color to match theme color chosen

--- src/components/Card/Style.ts
+++ src/components/Card/Style.ts
@@ -3,11 +3,11 @@ const tw = (classes: string) => classes;
 export const hoverImageStyle = tw("hover:scale-105 hover:cursor-pointer");
 
 export const imageStyle = tw(
-  "!max-w-none !w-80 h-auto !border-1 !border-purple-400 !m-0"
+  "!max-w-none !w-80 h-auto !border-1 !border-primary !m-0"
 );
 
 export const mobileImageStyle = tw(
-  "!max-w-none !w-60 h-auto !border-1 !border-purple-400 !m-0 shadow-2xl"
+  "!max-w-none !w-60 h-auto !border-1 !border-primary !m-0 shadow-2xl"
 );
 
 export const divStyle = tw("flex justify-center items-center");
